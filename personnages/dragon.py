413700dc4ca17f37cb4dc81e5141becbfa7bf939
Est-le-ch <129410185+Est-le-ch@users.noreply.github.com>
2023-03-30 17:56:50 +0200

    Update dragon.py

--- personnages/dragon.py
+++ personnages/dragon.py
@@ -1,1 +1,12 @@
+from random import *
 
+def dragon():
+    enemy_type = fire
+    hp = 150
+    if action == attack:
+        damage = randint(0,20)
+        hp = hp - damage
+        if hp > 0:
+            print("Le dragon a perdu", damage, "PV !")
+        elif hp <= 0:
+            print('Le dragoin est mort !')
