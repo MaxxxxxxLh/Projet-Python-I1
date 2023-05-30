820e5fc82edbff8af15b2920b8666d73b500395b
MaxxxxxxLh <122477345+MaxxxxxxLh@users.noreply.github.com>
2023-05-30 12:23:49 +0200

    .

--- jeu/background.py
+++ jeu/background.py
@@ -6,7 +6,8 @@ from guerrierHeros import guerrier
 from mageHeros import mage
 from chasseurHeros import chasseur
 from dragonClass import dragon
-
+from combat import *
+from tourJoueur import *
 pygame.init()
 
 
@@ -14,56 +15,67 @@ ecran = pygame.display.set_mode()
 
 x, y = ecran.get_size()
 
-#test avec cinq trolls et les attaquants
-trollUn = troll(x,y,70,75)
-trollDeux = troll(x,y,70,57)
-trollTrois = troll(x,y,70,39)
-trollQuatre = troll(x,y,70,21)
-trollCinq = troll(x,y,70,3)
 
-dragonneau = dragon(x,y,70,39)
 
-monGuerisseur = guerisseur(x,y,20,3)
-monMage = mage(x,y,20,70)
-monChasseur = chasseur(x,y,20,52)
-monGuerrier = guerrier(x,y,20,35)
 
-combattants = []
-
-combattants.append(monGuerisseur)
-combattants.append(monGuerrier)
-combattants.append(monMage)
-combattants.append(monChasseur)
-
-vagueTroll = []
-
-vagueTroll.append(dragonneau)
-'''
-vagueTroll.append(trollUn)
-vagueTroll.append(trollDeux)
-vagueTroll.append(trollTrois)
-vagueTroll.append(trollQuatre)
-vagueTroll.append(trollCinq)
-'''
-#fin des variables
 
 background = pygame.image.load("animation/images/BG.jpg")
 background = pygame.transform.scale(background, (x,y))
 
 continuer = True
+numeroDeVagues = 1
 
-while continuer:
-    
-    ecran.blit(background, (0,0))
-    for i in vagueTroll:
-        ecran.blit(i.resize,i.rect)
-    for j in combattants:
-        ecran.blit(j.resize,j.rect)
-    pygame.display.flip()
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            continuer = False
-            pygame.quit()
+compteur = 0
 
+def game(numeroDeVagues):
+    while numeroDeVagues !=5:
+        #initialisation des heros
+        monChasseur = chasseur(x,y,20,52)
+        monGuerrier = guerrier(x,y,20,35)
+        monGuerisseur = guerisseur(x,y,20,3)
+        monMage = mage(x,y,20,70)
+        combattants = {"chasseur": monChasseur, "guerrier": monGuerrier, "guerisseur": monGuerisseur, "mage" : monMage} #mettre les différents heros dans un dictionnaire
+        listeCombattants = list(combattants.values()) #créer une liste avec les combattants en vie
+        vaguesEnnemi = vagues(numeroDeVagues,x,y)
+        combat(vaguesEnnemi,compteur, continuer, listeCombattants)
+        if listeCombattants == []:
+            break
+        else:
+            numeroDeVagues += 1
+            for i in listeCombattants:
+                i.level += 1
+        
+
+    
+def combat(vaguesEnnemi,compteur, continuer, listeCombattants):
+    while continuer:
+        
+        ecran.blit(background, (0,0))
+        for i in vaguesEnnemi:
+            ecran.blit(i.resize,i.rect)
+        for j in listeCombattants:
+            ecran.blit(j.resize,j.rect)
+            
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                continuer = False
+                pygame.quit()
+                
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                try:
+                    clicked_sprites = [s for s in vaguesEnnemi if s.rect.collidepoint(pos)]
+                    for i in vaguesEnnemi:
+                        if clicked_sprites[0].pseudo == i.pseudo:  
+                            tourHeros(choixPersonnage(compteur,listeCombattants),listeCombattants, vaguesEnnemi,i)
+                    if vaguesEnnemi == [] or listeCombattants == []:
+                        continuer = False
+                except:
+                    pass
+                
+game(numeroDeVagues)
+
+        
 pygame.quit()
--- jeu/combat.py
+++ jeu/combat.py
@@ -1,69 +1,31 @@
-from chasseurHeros import *
-from guerisseurHeros import * 
-from guerrierHeros import *
-from mageHeros import *
 from trollClass import *
 from dragonClass import *
 
-#initialisation des heros
-monChasseur = chasseur(x,y,20,52)
-monGuerrier = guerrier(x,y,20,35)
-monGuerisseur = guerisseur(x,y,20,3)
-monMage = mage(x,y,20,70)
-
-
-
-combattants = {"chasseur": monChasseur, "guerrier": monGuerrier, "guerisseur": monGuerisseur, "mage" : monMage} #mettre les différents heros dans un dictionnaire
-listeCombattants = list(combattants.values()) #créer une liste avec les combattants en vie
-
-
-
-
-
-
-
-
-def cible(ennemis): #choix de l'ennemi à attaquer
-    if len(ennemis) != 1: #choisir si plusieurs ennemis
-        print("Il y a "+ str(len(ennemis))+" ennemis")
-        print("Qui voulez-vous attaquer?")
-        print("Veuillez indiquer un nombre entre 1 et " + str(len(ennemis)))
-        a = True
-        while a:
-            try:
-                choix = int(input(""))
-            except:
-                print("Veuillez entrer un nombre seulement!!!")
-            if 1 > choix or len(ennemis) < choix:
-                print("Veuillez saisir un nombre compris entre 1 et "+ str(len(ennemis)))
-                choix = input("")
-            elif choix <= len(ennemis) and choix >= 1:
-                a = False
-    else:
-        choix = 1
-    return choix-1
-
-
-def tourHeros(allies, ennemis): #attaquer les trolls
-    for i in allies:
-        if ennemis == []:
-            break
-        if i.estEnVie():
-            if i.type == "mage":
-                a = ennemis [cible(ennemis)]
-                i.attaque(ennemis,a)
+def tourHeros(attaquant,allies, ennemis, cibleEnnemis): #attaquer les trolls
+    if attaquant.estEnVie():
+        if attaquant.type == "mage":
+            attaquant.attaque(ennemis,cibleEnnemis)
+        else:
+            attaquant.attaque(cibleEnnemis)
+            print(cibleEnnemis.PV)
+        if cibleEnnemis.estEnVie():
+            if cibleEnnemis.type == "dragon":
+                cibleEnnemis.attaque(allies, attaquant)
             else:
-                a=ennemis[cible(ennemis)]
-                i.attaque(a)
-            if not a.estEnVie():
-                ennemis.remove(a)
-            if i.type == "dragon":
-                a.attaque(allies, i)
-            else:
-                a.attaque(i)
-            if not i.estEnVie():
-                allies.remove(i)
+                cibleEnnemis.attaque(attaquant)
+                print(attaquant.PV)
+        else:
+            ennemis.remove(cibleEnnemis)
+        if not attaquant.estEnVie():
+            allies.remove(attaquant)
         
+        
+        
+def choisirEnnemis(cible, vague):
+    if cible.type == "ennemi":
+        for j in vague:
+            if j.pseudo == cible.pseudo:
+                return j
 
 
 
@@ -71,42 +33,36 @@ def tourHeros(allies, ennemis): #attaquer les trolls
 
 
 
-def combat(attaquant, compteur):
-    vagueEnnemis = vagues(compteur)
-    while listeCombattants != [] and vagueEnnemis != []:
-        tourHeros(attaquant, vagueEnnemis)
-    if listeCombattants != []:
-        for i in listeCombattants:
-            i.level += 1
+def combat(attaquantListe, cible,vagueEnnemis):
+    while attaquantListe != [] and vagueEnnemis != []:
+        for j in attaquantListe:
+            tourHeros(j,attaquantListe, vagueEnnemis,cible)
+    if attaquantListe != []:
+        for k in attaquantListe:
+            k.level += 1
 
 
 
-def vagues(compteur):
+def vagues(compteur,x,y):
     vagueTroll = []
     if compteur >= 1 and compteur <= 3:
-        trollUn = troll()
-        trollDeux = troll()
-        trollTrois = troll()
+        trollUn = troll(x,y,70,57,"troll un")
+        trollDeux = troll(x,y,70,39, "troll deux")
+        trollTrois = troll(x,y,70,21, "troll trois")
         vagueTroll.append(trollUn)
         vagueTroll.append(trollDeux)
         vagueTroll.append(trollTrois)
         if compteur > 1 and compteur <= 3:
-            trollQuatre = troll()
+            trollQuatre = troll(x,y,70,75, "troll quatre")
             vagueTroll.append(trollQuatre)
             if compteur == 3:
-                trollCinq = troll()
+                trollCinq = troll(x,y,70,3, "troll cinq")
                 vagueTroll.append(trollCinq)
     if compteur == 4:
-        bossDragon = dragon()
+        bossDragon = dragon(x,y,70,39,"dragonneau")
         vagueTroll.append(bossDragon)
     return vagueTroll
 
 
 
-
-
-def jeu():
-    for compteur in range(1,5):
-        combat(listeCombattants, compteur)
-    
 
